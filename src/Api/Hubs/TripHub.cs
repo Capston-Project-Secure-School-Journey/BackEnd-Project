54b37dcb9a618b91b2cf6da816a2930bf8ef5ebb
Pham Van Tien Truong <tpham1467@gmail.com>
2025-06-09 22:27:34 +0700

    feat: enhance TripHub with driver location updates and improved logging

--- src/Api/Hubs/TripHub.cs
+++ src/Api/Hubs/TripHub.cs
@@ -1,7 +1,11 @@
+using Api.Attributes;
 using Api.Common.Enums;
+using Api.Extensions;
 using Api.Security.CurrentUserProvider;
+using Api.Services.DriverSchoolTripService;
 using Api.Services.ParentSchoolTripService;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace Api.Hubs;
 
@@ -14,7 +18,9 @@ public interface ITypedHubClient
 public class TripHub(
     ILogger<TripHub> logger,
     ICurrentUserProvider currentUserProvider,
-    IParentSchoolTripService parentSchoolTripService) : Hub<ITypedHubClient>
+    IParentSchoolTripService parentSchoolTripService,
+    IDriverSchoolTripService driverSchoolTripService,
+    IMemoryCache memoryCache) : Hub<ITypedHubClient>
 {
     private static readonly ConnectionMapping<SocketIdentity> Connections = new();
 
@@ -27,13 +33,13 @@ public class TripHub(
         );
         Connections.Add(key, Context.ConnectionId);
 
-        logger.LogInformation($"New Connection: {key}");
+        logger.LogInformation("New Connection: {key}", key);
         return base.OnConnectedAsync();
     }
 
     public override Task OnDisconnectedAsync(Exception? exception)
     {
-        logger.LogInformation($"Disconnect: {exception?.Message}");
+        logger.LogInformation("Disconnect: {Message}", exception?.Message);
         try
         {
             var currentUser = currentUserProvider.GetCurrentUser();
@@ -42,7 +48,7 @@ public class TripHub(
                 currentUser.UserType
             );
             Connections.Remove(key, Context.ConnectionId);
-            logger.LogInformation($"Disconnect: {key}");
+            logger.LogInformation("Disconnect: {key}", key);
         }
         catch (Exception)
         {
@@ -53,13 +59,37 @@ public class TripHub(
     }
 
     [HubMethodName("subscribe-Trip-room")]
+    [Authorize(UserType.Parent)]
     public async Task SubscribeTripRoom(Guid tripId)
     {
         var currentUser = currentUserProvider.GetCurrentUser();
-        if (currentUser.UserType == UserType.Parent &&
-            await parentSchoolTripService.HasInProgressShuttle(currentUser.UserId, tripId))
+        if (await parentSchoolTripService.HasInProgressShuttle(currentUser.UserId, tripId))
             await Groups.AddToGroupAsync(Context.ConnectionId, tripId.ToString());
         else
-            logger.LogInformation($"Can't subscribe trip room: {tripId}");
+            logger.LogInformation("Can't subscribe trip room: {tripId}", tripId);
+    }
+
+    [HubMethodName("update-trip-location")]
+    [Authorize(UserType.Driver)]
+    public async Task UpdateTripLocation(double latitude, double longitude)
+    {
+        var currentUser = currentUserProvider.GetCurrentUser();
+        logger.LogInformation("UpdateTripLocation called by {driverId}", currentUser.UserId);
+        
+        var key = $"TripHub_{currentUser.UserId}";
+        if (!memoryCache.TryGetValue(key, out Guid tripId))
+        {
+            var trip = await driverSchoolTripService.GetCurrentShuttleScheduleByDriver(currentUser.UserId);
+            memoryCache.Set(key, trip.Id, TimeSpan.FromMinutes(5));
+
+            driverSchoolTripService
+                .UpdateCurrentAddress(tripId, currentUser.UserId, latitude, longitude)
+                .FireAndForget((ex) => logger.LogError(ex, "DriverSchoolTripService.UpdateCurrentAddress"));
+        }
+
+        Clients
+            .Group(tripId.ToString())
+            .SendDriverAddress(tripId, latitude, longitude)
+            .FireAndForget((ex) => logger.LogError(ex, "TripHub.SendDriverAddress"));
     }
 }
